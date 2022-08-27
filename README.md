0d61757c2031fede2ac6f38c69b7a7519ec232ee
Boranity <107348321+boranityy@users.noreply.github.com>
2022-08-27 13:44:51 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# CLI Tureng
+A basic tool that only displays translated words
+
+## Install Modules
+```pip3 install -r requirements.txt```
---
+++ tureng.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup as bs4
+
+word = input('Enter Words: ')
+
+translate = requests.get("https://tureng.com/tr/turkce-ingilizce/"+word)
+soup = bs4(translate.content, "html.parser")
+
+def wordstr():
+    buffer = []
+    for tr in soup.find_all('td', 'tr ts'):
+        buffer.append(''.join(tr.findAll(text=True)))
+    return "\n".join(buffer) 
+
+def wordsen():
+    buffer = []
+    for en in soup.find_all('td', 'en tm'):
+        buffer.append(''.join(en.findAll(text=True)))
+    return "\n".join(buffer)
+
+question = input('''\033[36m
+1-) Show the Turkish translation
+2-) Show the English translation
+
+\033[96mSelect:\033[0m ''')
+
+if question == '1':
+    print(f"\033[91m{wordstr()}\033[0m")
+
+elif question == '2':
+    print(f"\033[91m{wordsen()}\033[0m")
